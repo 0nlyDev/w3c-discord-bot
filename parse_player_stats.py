f034acf792e11fe05ddf38c8899726395ee45ad9
0nlyDev <entr0py.pi3@gmail.com>
2023-10-27 01:48:59 +0200

    Removing redudent code

--- parse_player_stats.py
+++ parse_player_stats.py
@@ -1,38 +1,7 @@
-var = {
-    "race": 0,
-    "gameMode": 1,
-    "gateWay": 20,
-    "playerIds": [
-        {
-            "name": "Colorado16",
-            "battleTag": "Colorado16#11383"
-        }
-    ],
-    "season": 16,
-    "id": "16_Colorado16#11383@20_GM_1v1_RnD",
-    "mmr": 1594,
-    "rankingPoints": 27.21992813571109,
-    "rank": 21,
-    "leagueId": 32,
-    "leagueOrder": 5,
-    "division": 1,
-    "quantile": 0.72833616,
-    "rankingPointsProgress": {
-        "rankingPoints": 0,
-        "mmr": 0
-    },
-    "wins": 4,
-    "losses": 1,
-    "games": 5,
-    "winrate": 0.8
-}
-import math
-
 from w3c_endpoints.player_stats import RACES
 from w3c_endpoints.active_modes import get_game_mode_from_id
 
 
-# w/l 3:2
 def parse_player_stats(player_stats):
     message = '**w3champions player stats by game mode:**\n'
     for player_stat in player_stats:
@@ -59,7 +28,5 @@ def parse_player_stats(player_stats):
 def convert_win_rate_to_percentage(num):
     if 0 <= num <= 1:
         return f"{num * 100:.1f}%"
-    # else:
-    #     raise ValueError("The number must be between 0 and 1, inclusive.")
     else:
         return 0
