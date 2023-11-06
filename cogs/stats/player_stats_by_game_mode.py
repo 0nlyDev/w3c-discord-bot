202c713ff234ccf65789fc7dd2ae471dac47125d
0nlyDev <entr0py.pi3@gmail.com>
2023-11-07 00:12:03 +0100

    /player_stats_by_game_mode command moved to cogs

--- cogs/stats/player_stats_by_game_mode.py
+++ cogs/stats/player_stats_by_game_mode.py
@@ -1,17 +1,98 @@
+import json
+
 import discord
 from discord import app_commands
 from discord.ext import commands
 
-from responses import responses
+from player_stats_embed import get_player_stats_embed
+from w3c_endpoints.player_search import player_search
+from w3c_endpoints.player_stats import get_player_stats
 
 
 class PlayerStatsByGameMode(commands.Cog):
     def __init__(self, client: commands.Bot):
         self.client = client
+        with open('assets/emojis.json', 'r', encoding="utf-8") as file:
+            self.emojis = json.load(file)
 
     @app_commands.command(name='player_stats_by_game_mode', description='Get player\'s statistics by game mode.')
-    async def help(self, interaction: discord.Interaction):
-        await interaction.response.send_message(content='stats...', ephemeral=True)
+    async def player_stats_by_game_mode(self,
+                                        interaction,
+                                        player_name: str,
+                                        region: str = None,
+                                        game_mode: str = None,
+                                        race: str = None,
+                                        season: str = None):
+
+        search_results = player_search(player_name)
+        response, view = None, None
+        if search_results:
+            if len(search_results) == 1:
+                bnet_tag = search_results[0].split(' ')[0]
+                player_stats = get_player_stats(bnet_tag, region, game_mode, race, season)
+                response, view = get_player_stats_embed(player_stats, bnet_tag)
+            else:
+                response, view = PlayerSearchMenu(player_name, search_results, region, game_mode, race, season), None
+        if response:
+            if hasattr(response, 'children') and len(response.children) > 0:  # player_name
+                await interaction.response.send_message(
+                    '🌌 From the depths of the Dark Portal, select your champion below:', view=response, ephemeral=True)
+            else:
+                if view:
+                    await interaction.response.send_message(embed=response, view=view, ephemeral=True)
+                else:
+                    await interaction.response.send_message(embed=response, ephemeral=True)
+        else:
+            await interaction.response.send_message(
+                "🌌 In the vastness beyond the Dark Portal, this champion remains a mystery.", ephemeral=True)
+
+
+class PlayerSearchMenu(discord.ui.View):
+    def __init__(self, player_name, search_results, region, game_mode, race, season):
+        super().__init__()
+        self.add_item(PlayerSearchSelect(player_name, search_results, region, game_mode, race, season))
+
+
+class PlayerSearchSelect(discord.ui.Select):
+    def __init__(self, player_name, search_results, region, game_mode, race, season):
+        self.player_name, self.search_results, self.region, self.game_mode, self.race, self.season = (
+            player_name, search_results, region, game_mode, race, season)
+        options = [discord.SelectOption(label=player, value=player) for player in search_results]
+        self.load_more_results_string = '🌀 Summon more champions from the depths...'
+        if len(options) > 0:
+            # w3c playerSearch endpoint is not reliable - sometimes returns less than 20 players
+            # when there are still more results to be shown. So we keep it if > 0 because we are not sure when is the
+            # end of the search, and we will relay on the result to determine end of search.
+            options.append(discord.SelectOption(label=self.load_more_results_string,
+                                                value=self.load_more_results_string))
+        super().__init__(placeholder='Champion manifest from the portal\'s depths:', options=options)
+
+    async def callback(self, interaction):
+        user_choice = interaction.data['values'][0]
+        if self.load_more_results_string == user_choice:
+            last_bnet_tag = next(i for i in reversed(self.search_results) if i != self.load_more_results_string)
+            new_search_results = player_search(self.player_name, last_bnet_tag)
+            print('self.search_results[-2]', self.search_results[-2])
+            if isinstance(new_search_results, list) and new_search_results:
+                new_menu_select = PlayerSearchMenu(
+                    self.player_name, new_search_results, self.region, self.game_mode, self.race, self.season)
+                await interaction.response.send_message('🌌 Through the Dark Portal, more champions emerge!',
+                                                        view=new_menu_select, ephemeral=True)
+            elif isinstance(new_search_results, list):
+                await interaction.response.send_message(
+                    '🌌 By the Light! It seems like we\'ve reached the end of our '
+                    'journey! No more champions emerge from the Dark Portal. Try '
+                    'with a different Champion name...', ephemeral=True)
+            else:
+                await interaction.response.send_message(new_search_results, ephemeral=True)
+        else:
+            bnet_tag = user_choice.split(' ')[0]
+            _player_stats = get_player_stats(bnet_tag, self.region, self.game_mode, self.race, self.season)
+            player_stats_embed, view = get_player_stats_embed(_player_stats, bnet_tag)
+            if view:
+                await interaction.response.send_message(embed=player_stats_embed, view=view, ephemeral=True)
+            else:
+                await interaction.response.send_message(embed=player_stats_embed, ephemeral=True)
 
 
 async def setup(client: commands.Bot):
